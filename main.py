c6924aa8a6297c226c1818c27dfc776611d81d72
WouterPennings <wouterpennings@gmail.com>
2021-12-30 20:42:03 +0100

    feat: max duration execution time

--- main.py
+++ main.py
@@ -2,9 +2,27 @@ import os
 from discord.ext import commands
 from dotenv import load_dotenv
 import subprocess
+import datetime
 
 PREFIX = '!'
+ENCODER = 'utf-8'
 bot = commands.Bot(command_prefix=PREFIX)
+
+def GetError(err):
+    err = err.decode(ENCODER)
+    if len(err) > 4000:
+        return err[0:100]
+    return err
+
+async def SendLoopResult(context, stdout, stderr):
+    output = GetError(stdout)
+    error = GetError(stderr)
+    if stderr and stdout:
+        await context.send("```\n{}\n{}```".format(error, output))
+    elif stderr:
+        await context.send("```\n{}```".format(error)) 
+    else:
+        await context.send("```\n{}```".format(output))
 
 @bot.event
 async def on_ready():
@@ -16,16 +34,17 @@ async def run(context, *, code):
     file.write(code)
     file.close()
 
-    process = subprocess.Popen(['loop.exe', 'loop.loop'],
-                    stdout=subprocess.PIPE, 
-                    stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    if stderr and stdout:
-        await context.send("```\n{}\n{}```".format(stderr.decode('utf-8'), stdout.decode('utf-8')))
-    elif stderr:
-        await context.send("```\n{}```".format(stderr.decode('utf-8'))) 
-    else:
-        await context.send("```\n{}```".format(stdout.decode('utf-8')))
+    process = subprocess.Popen(['loop.exe', 'loop.loop'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    try:
+        stdout, stderr = process.communicate(timeout=1)
+        await SendLoopResult(context, stdout, stderr)
+    except subprocess.TimeoutExpired:
+        process.kill()
+        stdout, stderr = process.communicate()
+        await SendLoopResult(context, stdout, stderr)
+    
+    print("Succesfully executed Loop code")
 
 @bot.command()
 async def Willie(context):
@@ -34,3 +53,4 @@ async def Willie(context):
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 bot.run(TOKEN)
+
