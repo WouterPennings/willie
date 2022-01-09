acd7005b4f7d6506f593168150c3c8d86fcbae2f
WouterPennings <wouterpennings@gmail.com>
2022-01-09 22:21:08 +0100

    ssh-test

--- constants.py
+++ constants.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-ENCODER = 'utf-8'
 LOOP_COMPILER = "loop.exe"
 DOWNLOAD_URL = "https://cdn.looplang.org/prerelease/185_Windows_AMD64_loop_0.1.0.exe"
 PREFIX = '!'
